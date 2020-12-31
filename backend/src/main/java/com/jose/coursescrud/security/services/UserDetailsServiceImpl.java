e03cb3c6e81be0ce119c5883a2b5e4d3725105a2
joselfv1979 <61271903+joselfv1979@users.noreply.github.com>
2020-12-31 10:47:15 +0100

    Update UserDetailsServiceImpl.java

--- backend/src/main/java/com/jose/coursescrud/security/services/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/jose/coursescrud/security/services/UserDetailsServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-// CLASE QUE REALIZA EL SERVICIO DE AUTENTICACIÓN
+// CLASE SERVICIO QUE CARGA DATOS ESPECÍFICOS DEL USUARIO
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
